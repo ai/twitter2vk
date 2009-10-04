6cb39c6785025bc8ef344bd09858b5b34986fa50
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-05 01:08:29 +0400

    Add help message to installer

--- twitter2vk.rb
+++ twitter2vk.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
 # Console tool to create new config.
+
+if '--help' == ARGV.first or '-h' == ARGV.first
+  puts 'Create config and cron task to repost Twitter statuses to VK.com'
+  exit
+end
