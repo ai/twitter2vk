1d3a084df4b35478728985802a5a6f29f6827afc
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-05 01:42:12 +0400

    Add code to get VK session ID and Twitter name

--- twitter2vk.rb
+++ twitter2vk.rb
@@ -1,7 +1,27 @@
 #!/usr/bin/env ruby
 # Console tool to create new config.
 
+require 'rubygems'
+require 'highline/import'
+require 'net/http'
+
 if '--help' == ARGV.first or '-h' == ARGV.first
-  puts 'Create config and cron task to repost Twitter statuses to VK.com'
+  say 'Create config and cron task to repost Twitter statuses to VK.com'
   exit
 end
+
+HighLine.track_eof = false
+
+email    = ask('VK e-mail: ')
+password = ask('VK password: ') { |q| q.echo = false }
+
+resource = Net::HTTP.post_form(URI.parse('http://login.vk.com/'), 
+    { 'email' => email, 'pass' => password, 'vk' => '', 'act' => 'login' })
+
+if resource.body.empty?
+  say 'Error: VK e-mail or password is incorrect'
+  exit
+end
+session_id = resource.body.match(/value='([a-z0-9]+)'/)[1]
+
+twitter = ask('Twitter name: ')
