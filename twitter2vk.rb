869b11e7c9e487e45c76c7eb68b45668be77ca41
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-05 02:43:56 +0400

    Add code to create cron task

--- twitter2vk.rb
+++ twitter2vk.rb
@@ -7,6 +7,7 @@ require 'highline/import'
 require 'fileutils'
 require 'net/http'
 require 'yaml'
+require 'pathname'
 
 trap('INT') do
   puts
@@ -14,7 +15,7 @@ trap('INT') do
 end
 
 if '--help' == ARGV.first or '-h' == ARGV.first
-  say 'Create config and cron task to repost Twitter statuses to VK.com'
+  say 'Create config and cron task to repost Twitter statuses to VK.com.'
   exit
 end
 
@@ -28,17 +29,23 @@ password = ask('VK password: ') { |q| q.echo = false }
 resource = Net::HTTP.post_form(URI.parse('http://login.vk.com/'), 
     { 'email' => email, 'pass' => password, 'vk' => '', 'act' => 'login' })
 if resource.body.empty?
-  say 'Error: VK e-mail or password is incorrect'
+  say 'Error: VK e-mail or password is incorrect.'
   exit
 end
 
 config['vk_session'] = resource.body.match(/value='([a-z0-9]+)'/)[1]
 config['twitter']    = ask('Twitter name: ')
 
-config['exclude'] = ['#novk', /@[\w\d]/]
-config['include'] = ''
+config['exclude'] = ['#novk', /@\w/]
+config['include'] = nil
 
 path = ask('Config path: ') { |q| q.default = "./#{config['twitter']}.yml" }
 
 File.open(path, 'w') { |io| io << config.to_yaml }
 FileUtils.chmod 0700, path
+
+reposter = Pathname.new(__FILE__).dirname.expand_path + 'twitter2vk_reposter.rb'
+period = ask('Check period in minutes: ') { |q| q.default = 5 }
+
+`echo "#{period} * * * * #{reposter} #{File.expand_path(path)}" | crontab -`
+say 'Config and cron task are created.'
