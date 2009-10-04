60e5225b797d2f2d4e979dfeac10a73f6eca03ec
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-05 02:15:27 +0400

    Add code to get path and create YAML config

--- twitter2vk.rb
+++ twitter2vk.rb
@@ -4,16 +4,19 @@
 require 'rubygems'
 require 'highline/import'
 require 'net/http'
+require 'yaml'
+
+trap('INT') do
+  puts
+  exit
+end
 
 if '--help' == ARGV.first or '-h' == ARGV.first
   say 'Create config and cron task to repost Twitter statuses to VK.com'
   exit
 end
 
-trap('INT') do
-  puts
-  exit
-end
+config = {}
 
 HighLine.track_eof = false
 
@@ -22,11 +25,17 @@ password = ask('VK password: ') { |q| q.echo = false }
 
 resource = Net::HTTP.post_form(URI.parse('http://login.vk.com/'), 
     { 'email' => email, 'pass' => password, 'vk' => '', 'act' => 'login' })
-
 if resource.body.empty?
   say 'Error: VK e-mail or password is incorrect'
   exit
 end
-session_id = resource.body.match(/value='([a-z0-9]+)'/)[1]
 
-twitter = ask('Twitter name: ')
+config['vk_session'] = resource.body.match(/value='([a-z0-9]+)'/)[1]
+config['twitter']    = ask('Twitter name: ')
+
+config['exclude'] = ['#novk', /@[\w\d]/]
+config['include'] = ''
+
+path = ask('Config path: ') { |q| q.default = "./#{config['twitter']}.yml" }
+
+File.open(path, 'w') { |io| io << config.to_yaml }
