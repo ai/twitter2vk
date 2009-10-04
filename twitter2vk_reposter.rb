1cee747a0c7eb8ba11ae1368b04a61c71d98d60b
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-05 02:29:39 +0400

    Add help message to reposter

--- twitter2vk_reposter.rb
+++ twitter2vk_reposter.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
 # Check new Twitter statuses and repost it to VK.
+
+if ARGV.empty? or '--help' == ARGV.first or '-h' == ARGV.first
+  puts 'Usage: twitter2vk_reposter.rb CONFIG'
+  puts 'Repost Twitter statuses to VK.com. Call twitter2vk script to create ' +
+       'config and add cron task.'
+  exit
+end
