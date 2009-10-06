7a950f1dd9743caf4129f519c348170b7fab25b4
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-10-06 18:25:53 +0400

    Add code to get last messages from Twitter

--- twitter2vk_reposter.rb
+++ twitter2vk_reposter.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 # Check new Twitter statuses and repost it to VK.
 
+require 'rubygems'
+require 'json'
+
 require 'yaml'
+require 'open-uri'
 
 if ARGV.empty? or '--help' == ARGV.first or '-h' == ARGV.first
   puts 'Usage: twitter2vk_reposter.rb CONFIG'
@@ -14,4 +18,17 @@ config = YAML.load_file(ARGV.first)
 
 last_message = if File.exists? config['last_message']
   File.read(config['last_message']).strip
+end
+
+query = last_message ? "since_id=#{last_message}" : 'count=1'
+request = open('http://twitter.com/statuses/user_timeline/' +
+               "#{config['twitter']}.json?#{query}")
+statuses = JSON.parse(request.read.to_s)
+
+unless statuses.empty?
+  statuses.each do |status|
+    
+  end
+  
+  File.open(config['last_message'], 'w') { |io| io << statuses.first['id'] }
 end
